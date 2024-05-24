61b587f4f54184dd48b466389516a0ad360bc583
Bill Croteau <bill@tunko.com>
2024-05-23 23:47:17 -0400

    Initial commit

---
+++ api.js
@@ -0,0 +1,20 @@
+const express = require("express");
+import serverless from "serverless-http";
+
+const api = express();
+
+const router = Router();
+
+const mySecret = process.env.MY_SUPER_SECRET;
+console.log(mySecret);
+
+router.get("/secret", (req, res) => {
+  res.json({
+    message: "Shhh... this object contains my biggest secret.",
+    mySecret: `${mySecret}`,
+  });
+});
+
+api.use("/api/", router);
+
+export const handler = serverless(api);
